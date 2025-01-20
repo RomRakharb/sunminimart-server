242cf14a54a76a5c90f7367a9d28cbb1fdd6d68b
Rom Rakharb <romr.dev@proton.me>
2025-01-21 00:10:47 +0700

    start creating db

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-use sqlx::mysql::MySqlPoolOptions;
+use sqlx::{mysql::MySqlPoolOptions, query, Executor, MySqlPool};
 
 #[tokio::main]
-async fn main() {
-    let pool = MySqlPoolOptions::new().max_connections(5);
-    println!("Hello, world!");
+async fn main() -> sqlx::Result<()> {
+    let _ = init();
+    Ok(())
+}
+
+async fn init() -> sqlx::Result<()> {
+    let pool = MySqlPoolOptions::new()
+        .connect("mysql://rom:mypassword@localhost/")
+        .await?;
+    pool.execute(sqlx::query("CREATE DATABASE IF NOT EXISTS sunminimart;"))
+        .await?;
+    Ok(())
 }
